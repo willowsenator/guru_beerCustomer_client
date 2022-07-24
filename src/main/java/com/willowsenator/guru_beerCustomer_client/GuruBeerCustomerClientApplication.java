4a8a488e01c0d7858acec9bc2a094524a80bc713
willowsenator <53278383+willowsenator@users.noreply.github.com>
2022-07-24 19:52:45 +0200

    Init commit

---
+++ src/main/java/com/willowsenator/guru_beerCustomer_client/GuruBeerCustomerClientApplication.java
@@ -0,0 +1,13 @@
+package com.willowsenator.guru_beerCustomer_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruBeerCustomerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruBeerCustomerClientApplication.class, args);
+	}
+
+}
